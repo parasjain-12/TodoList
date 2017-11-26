f3806b111b97b6a7a43c19d335fe875851eaf0ae
parasjain-12 <jainparas249@gmail.com>
2017-11-26 17:22:22 +0530

    Create todo.js

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+
+$("ul").on("click", "li",function(){
+	$(this).toggleClass("completed");
+    
+	
+});
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+
+	});
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(e){
+    if(e.which===13){
+    	var TodoText = ($(this).val());
+    	$(this).val("");
+    	$("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> "+ TodoText + "</li>");
+    }
+});
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
